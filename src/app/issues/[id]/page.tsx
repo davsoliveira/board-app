779030abb86ed1575ff2288f127c6bc146ee9d44
Davi Oliveira <davi.ol.santos@gmail.com>
2026-02-23 21:14:39 -0300

    refactor: adicionar partial pre-rendering suspense API

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -3,7 +3,9 @@ import { getIssue } from "@/http/get-issue";
 import { MoveLeftIcon, ArchiveIcon, ThumbsUpIcon } from "lucide-react";
 import type { Metadata } from "next";
 import Link from "next/link";
-import { IssueCommentsList } from "./issue-comments/issue-coments-list";
+import { IssueCommentsList } from "./issue-comments/issue-comments-list";
+import { Suspense } from "react";
+import { IssueCommentsSkeleton } from "./issue-comments/issue-comments-skeleton";
 
 interface IssuePageProps {
   params: Promise<{ id: string }>;
@@ -68,7 +70,9 @@ export default async function IssuePage({ params }: IssuePageProps) {
         <form />
 
         <div className="mt-3">
-          <IssueCommentsList issueId={issue.id} />
+          <Suspense fallback={<IssueCommentsSkeleton />}>
+            <IssueCommentsList issueId={issue.id} />
+          </Suspense>
         </div>
       </div>
     </main>
